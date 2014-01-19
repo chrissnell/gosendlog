c2fe44f399d3f2453a72781e87e6c9f9a0c86d8b
Christopher Snell <chris.snell@gmail.com>
2014-01-19 00:32:00 -0800

    Some reformatting and clean-up

--- gosendlog.go
+++ gosendlog.go
@@ -9,6 +9,41 @@ import (
 	"os"
 )
 
+// Syslog priorities from /usr/include/sys/syslog.h
+var priorityStrings = map[string]syslog.Priority{
+	"emerg":   0,
+	"alert":   1,
+	"crit":    2,
+	"err":     3,
+	"warning": 4,
+	"notice":  5,
+	"info":    6,
+	"debug":   7,
+}
+
+var facilityStrings = map[string]syslog.Priority{
+	"kern":     0 << 3,
+	"user":     1 << 3,
+	"mail":     2 << 3,
+	"daemon":   3 << 3,
+	"auth":     4 << 3,
+	"syslog":   5 << 3,
+	"lpr":      6 << 3,
+	"news":     7 << 3,
+	"uucp":     8 << 3,
+	"cron":     9 << 3,
+	"authpriv": 10 << 3,
+	"ftp":      11 << 3,
+	"local0":   16 << 3,
+	"local1":   17 << 3,
+	"local2":   18 << 3,
+	"local3":   19 << 3,
+	"local4":   20 << 3,
+	"local5":   21 << 3,
+	"local6":   22 << 3,
+	"local7":   23 << 3,
+}
+
 func ProcessLinesFromReader(r io.Reader, processFunc func(string)) {
 	br := bufio.NewReader(r)
 	for line, err := br.ReadString('\n'); err == nil; line, err = br.ReadString('\n') {
@@ -25,42 +60,6 @@ func main() {
 
 	var readFromStdin bool
 
-	var prio map[string]syslog.Priority
-	prio = make(map[string]syslog.Priority)
-	var facil map[string]syslog.Priority
-	facil = make(map[string]syslog.Priority)
-
-	// Syslog priorities from /usr/include/sys/syslog.h
-	prio["emerg"] = 0
-	prio["alert"] = 1
-	prio["crit"] = 2
-	prio["err"] = 3
-	prio["warning"] = 4
-	prio["notice"] = 5
-	prio["info"] = 6
-	prio["debug"] = 7
-
-	facil["kern"] = 0 << 3
-	facil["user"] = 1 << 3
-	facil["mail"] = 2 << 3
-	facil["daemon"] = 3 << 3
-	facil["auth"] = 4 << 3
-	facil["syslog"] = 5 << 3
-	facil["lpr"] = 6 << 3
-	facil["news"] = 7 << 3
-	facil["uucp"] = 8 << 3
-	facil["cron"] = 9 << 3
-	facil["authpriv"] = 10 << 3
-	facil["ftp"] = 11 << 3
-	facil["local0"] = 16 << 3
-	facil["local1"] = 17 << 3
-	facil["local2"] = 18 << 3
-	facil["local3"] = 19 << 3
-	facil["local4"] = 20 << 3
-	facil["local5"] = 21 << 3
-	facil["local6"] = 22 << 3
-	facil["local7"] = 23 << 3
-
 	destPtr := flag.String("dest", "", "Destination host <host:port>")
 	msgPtr := flag.String("msg", "", "Message <string>")
 	tagPtr := flag.String("tag", "", "Tag <string>")
@@ -70,8 +69,8 @@ func main() {
 
 	flag.Parse()
 
-	mappedPriority := prio[*prioPtr]
-	mappedFacility := facil[*facilPtr]
+	mappedPriority := priorityStrings[*prioPtr]
+	mappedFacility := facilityStrings[*facilPtr]
 
 	if *destPtr == "" {
 		log.Fatal("Must pass a destination host. Use -h for help.")
